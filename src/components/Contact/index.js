8287c5fe11782eac3e2b891a54d69e50c63a4954
Shinichi <miyakawashi@gmail.com>
2022-03-16 23:34:31 -0400

    contact form component implementation

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,21 +1,33 @@
 import React, { useState, useEffect } from 'react';
 
 export default function ContactForm() {
+    const [formState, setFormState] = useState({ name: '', email: '', message: ''});
+    const { name, email, message } = formState;
+
+    function handleChange(e){
+        setFormState({ ...formState, [e.target.name]: e.target.value });
+    };
+
+    function handleSubmit(e){
+        e.preventDefault();
+        console.log(formState);
+    };
+
     return (
         <section>
             <h1>Contact Me</h1>
-            <form id='contact-form'>
+            <form id='contact-form' onSubmit={handleSubmit}>
                 <div>
                     <label htmlFor='name'>Name:</label>
-                    <input name='name'type='text'></input>
+                    <input name='name' type='text' defaultValue={name} onChange={handleChange}></input>
                 </div>
                 <div>
                     <label htmlFor='email'>Email:</label>
-                    <input name='email' type='email'></input>
+                    <input name='email' type='email' defaultValue={email} onChange={handleChange}></input>
                 </div>
                 <div>
                     <label htmlFor='message'>Message:</label>
-                    <textarea name='message' rows='5'></textarea>
+                    <textarea name='message' rows='5' defaultValue={message} onChange={handleChange}></textarea>
                 </div>
                 <button type='submit'>Submit</button>
             </form>
